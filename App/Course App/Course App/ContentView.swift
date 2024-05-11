bd88273ff9fc20421882fef5d53c9934b6f0cd7f
Christian <kiko22223@gmail.com>
2024-05-11 21:40:47 +0200

    FourthHomework

--- App/Course App/Course App/ContentView.swift
+++ App/Course App/Course App/ContentView.swift
@@ -4,16 +4,30 @@
 //
 //  Created by Christián on 26/04/2024.
 //
-
+import os
 import SwiftUI
 
 struct ContentView: View {
+    let fontSize: CGFloat = 100
+    let configuration = Configuration.default
+    let logger = Logger()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(Font.custom("Poppins-Regular", size: fontSize))
+        }.onAppear {
+            logger.info("View appeared")
+            logger.info("Our configuration is \(String(describing: configuration))")
+            let identifier: String = "[SYSTEM FONTS]"
+            for family in UIFont.familyNames as [String] {
+                debugPrint("\(identifier) FONT FAMILY : \(family)")
+                for name in UIFont.fontNames(forFamilyName: family) {
+                    debugPrint("\(identifier) FONT NAME : \(name)")
+                }
+            }
         }
         .padding()
     }
